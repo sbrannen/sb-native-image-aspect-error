52006da398cc5c8710892bc3b3cdc51042415b47
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-09-06 17:50:28 +0200

    Make sample application work in native image with CGLIB proxies

--- src/main/java/org/example/demo/service/ServiceImpl.java
+++ src/main/java/org/example/demo/service/ServiceImpl.java
@@ -1,6 +1,8 @@
 package org.example.demo.service;
 
+import org.example.demo.aspect.AutomaticLogger;
 
+@AutomaticLogger
 public class ServiceImpl implements Service {
   private final Integer constant;
 
